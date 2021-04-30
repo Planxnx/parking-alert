e71707a2dd7fc5f590a3d0826b756126d2a92076
Planxnx <60010465@kmitl.ac.th>
2021-04-30 10:40:27 +0700

    :sparkle: car not found screen component

---
+++ parking-alert-app/src/pages/Car/component/NotFound.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+const NotFoundScreen: React.FC = () => {
+  return (
+    <div>
+      <div className="absolute h-screen w-screen block bg-gray-400	 opacity-50 z-10"></div>
+      <div className="absolute h-screen w-screen block z-20">
+        <div className="h-screen w-screen flex items-center justify-center">
+          <div className="animate-bounce rounded-xl bg-purple-600 p-10">
+            <p className="font-bold text-5xl text-white">NotFound!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundScreen;
