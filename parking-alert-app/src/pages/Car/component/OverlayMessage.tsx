1f8501e6593cf787c2e0f70bd055fa55dd3e16d6
Planxnx <60010465@kmitl.ac.th>
2021-04-30 11:45:53 +0700

    :sparkles: super component!

--- parking-alert-app/src/pages/Car/component/OverlayMessage.tsx
+++ parking-alert-app/src/pages/Car/component/OverlayMessage.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-const NotFoundScreen: React.FC = () => {
+const OverlayMessage: React.FC<{
+  code?: string;
+  title: string;
+  subtitle?: string;
+}> = ({ code, title, subtitle }) => {
   return (
     <div>
       <div className="absolute h-screen w-screen block bg-gray-400	 opacity-50 z-10"></div>
       <div className="absolute h-screen w-screen block z-20">
         <div className="h-screen w-screen flex items-center justify-center">
           <div className="animate-bounce rounded-xl bg-purple-600 p-10">
-            <p className="font-bold text-5xl text-white">
-              404
-              <br />
-              NotFound!
-            </p>
+            <p className="font-bold text-5xl text-white">{code}</p>
+            <p className="font-bold text-5xl text-white">{title}</p>
+            <p className="font-normal text-2xl text-white">{subtitle}</p>
           </div>
         </div>
       </div>
@@ -18,4 +20,4 @@ const NotFoundScreen: React.FC = () => {
   );
 };
 
-export default NotFoundScreen;
+export default OverlayMessage;
